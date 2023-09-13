768f336362c2771d07922df8aed20d45096d5e70
marcela <marcelac2026@student.cis.hk.com>
2023-09-13 09:53:48 +0800

    commit

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,28 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
 
+        int age;
+        String firstName;
+        String favouriteFood;
 
+        System.out.println("Please enter your age");
+        age = scanner.nextInt();
 
+        System.out.println(age);
+
+        System.out.println("Please enter your first name");
+        firstName = scanner.next();
+
+        System.out.println(firstName);
+
+        System.out.println("Please enter your favourite food");
+        favouriteFood = scanner.next();
+
+        System.out.println(favouriteFood);
+
+        System.out.println("First name: " + firstName + "\nAge: " + age + "\nFavourite food: " + favouriteFood);
     }
 
 }
